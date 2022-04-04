0622f9540164fd865e2b4fbab817434d7dca004e
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2022-04-04 14:03:11 -0700

    test 3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import {k} from "./kaboom.js"
 // add a piece of text at position (120, 80)
 k.add([
     k.scale(2),
-    k.text("hello world"),
+    k.text("hellooo world"),
     k.pos(50, 80),
 ]);
 
