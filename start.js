557dd8799eea853fa7a24064fffb90fa486c9c1f
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2022-04-05 15:30:35 -0700

    restarted

--- start.js
+++ start.js
@@ -1,207 +1,181 @@
 import "./kaboom.js"
 
 
-scene("start", () => {
-    add([
-        scale(2),
-        text("start screen!"),
-        pos(50, 80),
-    ])
 
 
 
 
-// Extend our game with multiple scenes
 
-// Start game
-//kaboom()
 
-//Load assets
 loadSprite("bean", "/sprites/blackking.png")
 loadSprite("coin", "/sprites/blackking.png")
 loadSprite("spike", "/sprites/blackking.png")
 loadSprite("grass", "/sprites/blackking.png")
 loadSprite("ghosty", "/sprites/blackking.png")
 loadSprite("portal", "/sprites/blackking.png")
-// loadSound("score", "/sounds/score.mp3")
-// loadSound("portal", "/sounds/portal.mp3")
 
+// Extend our game with multiple scenes
 
+// Start game
+//kaboom()
+
+// Load assets
 
 
 const SPEED = 480
 
-// Design 2 levels
+
 const LEVELS = [
-	[
-		"@  ^ $$ >",
-		"=========",
-	],
-	[
-		"@   $   >",
-		"=   =   =",
-	],
+[
+"@  ^ $$ >",
+"=========",
+],
+[
+"@   $   >",
+"=   =   =",
+],
 ]
 
 // Define a scene called "game". The callback will be run when we go() to the scene
 // Scenes can accept argument from go()
 scene("game", ({ levelIdx, score }) => {
 
-	gravity(2400)
+gravity(2400)
 
-	// Use the level passed, or first level
-	const level = addLevel(LEVELS[levelIdx || 0], {
-		width: 64,
-		height: 64,
-		pos: vec2(100, 200),
-		"@": () => [
-			sprite("bean"),
-			area(),
-			body(),
+// Use the level passed, or first level
+const level = addLevel(LEVELS[levelIdx || 0], {
+width: 64,
+height: 64,
+pos: vec2(100, 200),
+"@": () => [
+sprite("bean"),
+area(),
+body(),
+origin("bot"),
+"player",
+],
+"=": () => [
+sprite("grass"),
+area(),
+solid(),
+origin("bot"),
+],
+"$": () => [
+sprite("coin"),
+area(),
+origin("bot"),
+"coin",
+],
+"^": () => [
+sprite("spike"),
+area(),
+origin("bot"),
+"danger",
+],
+">": () => [
+sprite("portal"),
+area(),
+origin("bot"),
+"portal",
+],
+})
 
-      scale(.3),
+// Get the player object from tag
+const player = get("player")[0]
 
-			origin("bot"),
-			"player",
-		],
-		"=": () => [
-			sprite("grass"),
-			area(),
-			solid(),
+// Movements
+onKeyPress("space", () => {
+if (player.isGrounded()) {
+player.jump()
+}
+})
 
-      scale(.3),
+onKeyDown("left", () => {
+player.move(-SPEED, 0)
+})
 
-			origin("bot"),
-		],
-		"$": () => [
-			sprite("coin"),
-			area(),
+onKeyDown("right", () => {
+player.move(SPEED, 0)
+})
 
-      scale(.3),
+player.onCollide("danger", () => {
+player.pos = level.getPos(0, 0)
+// Go to "lose" scene when we hit a "danger"
+go("lose")
+})
 
-			origin("bot"),
-			"coin",
-		],
-		"^": () => [
-			sprite("spike"),
-			area(),
+player.onCollide("coin", (coin) => {
+destroy(coin)
 
-      scale(.3),
+score++
+scoreLabel.text = score
+})
 
-			origin("bot"),
-			"danger",
-		],
-		">": () => [
-			sprite("portal"),
-			area(),
+// Fall death
+player.onUpdate(() => {
+if (player.pos.y >= 480) {
+go("lose")
+}
+})
 
-      scale(.3),
+// Enter the next level on portal
+player.onCollide("portal", () => {
 
-			origin("bot"),
-			"portal",
-		],
-	})
+if (levelIdx < LEVELS.length - 1) {
+// If there's a next level, go() to the same scene but load the next level
+go("game", {
+levelIdx: levelIdx + 1,
+score: score,
+})
+} else {
+// Otherwise we have reached the end of game, go to "win" scene!
+go("win", { score: score, })
+}
+})
 
-	// Get the player object from tag
-	const player = get("player")[0]
-
-	// Movements
-	onKeyPress("space", () => {
-		if (player.isGrounded()) {
-			player.jump()
-		}
-	})
-
-	onKeyDown("left", () => {
-		player.move(-SPEED, 0)
-	})
-
-	onKeyDown("right", () => {
-		player.move(SPEED, 0)
-	})
-
-	player.onCollide("danger", () => {
-		player.pos = level.getPos(0, 0)
-		// Go to "lose" scene when we hit a "danger"
-		go("lose")
-	})
-
-	player.onCollide("coin", (coin) => {
-		destroy(coin)
-
-	
-		score++
-		scoreLabel.text = score
-	})
-
-	// Fall death
-	player.onUpdate(() => {
-		if (player.pos.y >= 480) {
-			go("lose")
-		}
-	})
-
-	// Enter the next level on portal
-	player.onCollide("portal", () => {
-
-		if (levelIdx < LEVELS.length - 1) {
-			// If there's a next level, go() to the same scene but load the next level
-			go("game", {
-				levelIdx: levelIdx + 1,
-				score: score,
-			})
-		} else {
-			// Otherwise we have reached the end of game, go to "win" scene!
-			go("win", { score: score, })
-		}
-	})
-
-	// Score counter text
-	const scoreLabel = add([
-		text(score),
-		pos(12)
-	])
+// Score counter text
+const scoreLabel = add([
+text(score),
+pos(12)
+])
 
 })
 
 scene("lose", () => {
 
-	add([
-		text("You Lose"),
-		pos(12),
-	])
+add([
+text("You Lose"),
+pos(12),
+])
 
-	// Press any key to go back
-	onKeyPress(start)
-
+// Press any key to go back
+onKeyPress(start)
 
 })
 
 scene("win", ({ score }) => {
 
-	add([
-		text(`You grabbed ${score} coins!!!`, {
-			width: width(),
-		}),
-		pos(12),
-	])
+add([
+text(`You grabbed ${score} coins!!!`, {
+width: width(),
+}),
+pos(12),
+])
 
-	onKeyPress(start)
-
-})
-
+onKeyPress(start)
 
 })
-
-
-
 
 function start() {
-	// Start with the "game" scene, with initial parameters
-	go("game", {
-		levelIdx: 0,
-		score: 0,
-	})
+// Start with the "game" scene, with initial parameters
+go("game", {
+levelIdx: 0,
+score: 0,
+})
 }
 
 start()
+
+
+
+
